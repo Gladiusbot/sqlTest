2c9f9a3c90fc4a0ef361d823d88447129c73d577
GladiusBot <1067950735@qq.com>
2024-01-07 16:47:55 +0800

    parse csv and insert into db single thread

---
+++ CSVParser.cpp
@@ -0,0 +1,58 @@
+#include <cppconn/statement.h>
+#include <mysql_connection.h>
+#include <mysql_driver.h>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+int main() {
+  try {
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
+
+    const std::string db_host = "172.17.0.3";
+    const std::string db_user = "root";
+    const std::string db_password = "123456";
+    const std::string db_name = "BILLS";
+
+    driver = sql::mysql::get_mysql_driver_instance();
+    con = driver->connect(db_host, db_user, db_password);
+    con->setSchema(db_name);
+    stmt = con->createStatement();
+
+    // csv file
+    const std::string csv_file_path = "./csvFiles/data_1.csv";
+    std::ifstream csv_file(csv_file_path);
+    if (!csv_file.is_open()) {
+      std::cerr << "Failed to open CSV file." << std::endl;
+      return 1;
+    }
+
+    // parse
+    std::string line;
+    // skip first line
+    getline(csv_file, line);
+    while (getline(csv_file, line)) {
+      std::istringstream iss(line);
+      std::string column1, column2, column3;
+
+      getline(iss, column1, ',');
+      getline(iss, column2, ',');
+      getline(iss, column3, ',');
+
+      // insert into db
+      // TODO timeit
+      std::string query = "INSERT INTO BILLS VALUES('" + column1 + "', '" +
+                          column2 + "', '" + column3 + "')";
+      std::cout << query << std::endl;
+      stmt->execute(query);
+    }
+    std::cout << "Data insert finished." << std::endl;
+  } catch (sql::SQLException &e) {
+    std::cerr << "MySQL Errot: " << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
+}
